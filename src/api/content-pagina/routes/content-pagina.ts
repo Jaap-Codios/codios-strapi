5a3fac2e33eabd17b817cd642fe1f7cafb8a4e80
Jaap-Codios <jaap@codios.nl>
2024-11-14 16:00:34 +0100

    more fixes!

---
+++ src/api/content-pagina/routes/content-pagina.ts
@@ -0,0 +1,7 @@
+/**
+ * content-pagina router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::content-pagina.content-pagina');
---
+++ src/api/content-pagina/services/content-pagina.ts
@@ -0,0 +1,7 @@
+/**
+ * content-pagina service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::content-pagina.content-pagina');
