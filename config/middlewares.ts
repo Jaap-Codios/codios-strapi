63c7ce796c48b7f0d770df720748dd042ca1743f
Jaap-Codios <jaap@codios.nl>
2024-11-20 13:41:03 +0100

    vercel back in there

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -30,7 +30,27 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:8080', 'https://www.codios.nl', 'https://codios.nl', 'https://cms.codios.nl'],
+      // origin: ['http://localhost:8080', 'https://www.codios.nl', 'https://codios.nl', 'https://cms.codios.nl'],
+      origin: (ctx) => {
+        const origin = ctx.request.header.origin;
+        const allowedOrigins = [
+          'http://localhost:8080',
+          'https://www.codios.nl',
+          'https://codios.nl',
+          'https://cms.codios.nl',
+        ];
+
+        const allowedRegexOrigins = [/^https:\/\/.*\.vercel\.app$/]; // Vercel-subdomeinen toestaan
+
+        // Controleer of origin in statische lijst staat of matcht met een regex
+        if (
+          allowedOrigins.includes(origin) ||
+          allowedRegexOrigins.some((regex) => regex.test(origin))
+        ) {
+          return origin;
+        }
+        return false; // Blokkeer niet-goedgekeurde origins
+      },
       headers: '*',
       credentials: true,
     },
