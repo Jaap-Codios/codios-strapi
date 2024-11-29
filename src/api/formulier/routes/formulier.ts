6adf97ce74443ae00b89c1722e8f977637229e39
Jaap-Codios <jaap@codios.nl>
2024-11-29 11:51:15 +0100

    add formulier type

---
+++ src/api/formulier/routes/formulier.ts
@@ -0,0 +1,7 @@
+/**
+ * formulier router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::formulier.formulier');
---
+++ src/api/formulier/services/formulier.ts
@@ -0,0 +1,7 @@
+/**
+ * formulier service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::formulier.formulier');
